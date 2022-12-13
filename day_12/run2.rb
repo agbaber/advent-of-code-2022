30c629aac8b8be62a9259fc3031cbd97886aacca
Andrew Baber <baber@andrewbaber.com>
2022-12-12 21:58:56 -0500

    Day 12

--- day_12/run2.rb
+++ day_12/run2.rb
@@ -6,8 +6,8 @@ class Runner
     @map = []
     @coordinate_options = {}
     parse_input
-    @start_point = find_point('S')
-    @end_point = find_point('E')
+    @start_points = find_points('b') # this is totally cheating
+    @end_point = find_points('E').first
   end
 
   def run
@@ -15,13 +15,21 @@ class Runner
       row.each_with_index do |value, column_i|
         ac = get_available_coordinates(column_i, row_i, value)
         @coordinate_options[[row_i,column_i]] = ac
-
-        
       end
     end
     
+    shortest = 500
+    require 'pry';binding.pry
+    @start_points.each_with_index do |point, i|
+      puts "Checking point #{i} of #{@start_points.size}"
+      distance = BreadthFirstSearch.new(@coordinate_options, point).shortest_path_to(@end_point).size + 1
 
-    BreadthFirstSearch.new(@coordinate_options, @start_point).shortest_path_to(@end_point).size + 1
+      if distance < shortest
+        shortest = distance
+      end
+    end
+
+    puts shortest
   end
 
   private
@@ -56,18 +64,18 @@ class Runner
     ac
   end
 
-  def find_point(char)
-    point = nil
+  def find_points(char)
+    points = []
 
     @map.each_with_index do |row, i|
       idx = row.find_index(char)
 
       if idx
-        point = [i,idx]
+        points << [i,idx]
       end
     end
 
-    point
+    points
   end
 
   def parse_input
@@ -125,4 +133,4 @@ class Runner
 end
 
 Runner.new('input.txt').run
-# 489 too low
+# 487 too low

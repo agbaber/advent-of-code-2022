c771d5d18184dd8606af6d67f6fc2b4ead06db93
Andrew Baber <baber@andrewbaber.com>
2022-12-12 21:59:16 -0500

    Update 9

--- day_09/run.rb
+++ day_09/run.rb
@@ -14,14 +14,16 @@ class Runner
   def run
     @instructions.each do |direction, steps|
       puts "== #{direction} #{steps} =="
+      step = 0
       steps.times do
-        # puts "head_position: #{@head_position}"
-        # puts "tail_position: #{@tail_position}"
-
+        step += 1
         previous_head = @head_position.dup
         move_head(direction)
         check_and_move_tail(direction, previous_head)
-        # print_debug
+
+        # if [1,2,3].include?(step)
+          # print_debug
+        # end
       end
     end
 
@@ -34,15 +36,7 @@ class Runner
     @x_delta = @head_position[0].abs - @tail_position[0].abs
     @y_delta = @head_position[1].abs - @tail_position[1].abs
 
-    if @x_delta.abs == 1 
-      if @y_delta.abs == 1
-        true
-      else
-        false
-      end
-    else
-      false
-    end
+    @x_delta.abs == 1 && @y_delta.abs == 1
   end
 
   def check_and_move_tail(direction, previous_head)
@@ -76,9 +70,8 @@ class Runner
 
   def print_debug
     matrix = (Matrix.build(6, 6) {|row, col| '.' }).to_a
-    matrix[5][0] = 's'
 
-    # puts "mapping T to #{@tail_position[0]}x#{@tail_position[1]}"
+    matrix[5][0] = 's'
     if matrix[@tail_position[0]]
       matrix[@tail_position[0]][@tail_position[1]] = 'T'
     end
@@ -88,7 +81,6 @@ class Runner
     end
 
     puts "\n"
-    # require 'pry';binding.pry
     matrix.each do |r|
       puts r.join('')
     end
